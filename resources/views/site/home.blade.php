2957eedc031f004b0b9a5af6954b738875038151
Alan Sprecacenere <alan@qreate.it>
2016-04-27 00:34:01 +0200

    Stilato meglio il form di ricerca in home page

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -14,8 +14,8 @@
 				<span class="navbar-badge-text">Il gioco &egrave; vietato<br>ai minori di 18 anni</span>
 			</span>
 			<span class="hidden-sm-down">
-				<a class="btn btn-sm btn-primary" href="#">Iscriviti</a>
 				<a class="btn btn-sm btn-secondary-outline" href="#">Accedi</a>
+				<a class="btn btn-sm btn-primary" href="#">Iscriviti</a>
 			</span>
 		</div>
 	</div>
@@ -32,12 +32,12 @@
 						<div class="card-block hero-card-block">
 							<div class="row">
 								<div class="col-xs-offset-1 col-xs-10 col-md-7">
-									<h2>Cerca le sale da gioco più vicine a te, trova i jackpot più alti e vinci!</h2>
-									<p>Più di 5000 sale tra cui scegliere!</p>
-									<p class="m-t-1 hidden-md-up">
-										<a class="btn btn-secondary-outline" href="#">Accedi</a>
+									<h2>Cerca le sale da gioco più vicine a te, trova i jackpot più alti e&nbsp;vinci!</h2>
+									<p>Più di 5000 sale tra cui&nbsp;scegliere!</p>
+									<div class="m-t-1 hidden-md-up">
 										<a class="btn btn-primary" href="#">Iscriviti</a>
-									</p>
+										<a class="btn btn-secondary-outline" href="#">Accedi</a>
+									</div>
 								</div>
 								<div class="col-md-4">
 									<div class="logo"></div>
@@ -51,7 +51,7 @@
 								<div class="col-xs-offset-1 col-xs-10">
 
 									<div class="row">
-										<div class="col-md-4">
+										<div class="col-md-4 col-xl-5">
 											<div class="form-group">
 												<label class="initialism" for="form_query"><strong>Cosa</strong></label><br>
 												<input type="text" class="form-control form-control-lg" name="query" value="{{ $query }}" placeholder="Sto cercando&hellip;" id="form_query">
@@ -64,7 +64,7 @@
 												<input type="text" class="form-control form-control-lg" name="near" value="{{ $near }}" placeholder="Citt&agrave;" id="form_near">
 											</div>
 										</div>
-										<div class="col-md-3">
+										<div class="col-md-3 col-xl-2">
 											<div class="form-group">
 												<label class="hidden-sm-down">&nbsp;</label>
 												<button type="submit" class="btn btn-lg btn-block search-btn">
