2ea9beee3ef608d705b940c71785d864813bf27c
Alan Sprecacenere <alan@qreate.it>
2017-05-18 23:19:55 +0200

    Piccole fix qua e là

--- app/Http/Controllers/Site/DetailController.php
+++ app/Http/Controllers/Site/DetailController.php
@@ -24,7 +24,7 @@ class DetailController extends Controller
 
 		return view('site.venues.detail', [
 			'venue' => $venue,
-			'venue_category_string' => $venue_category_names->implode(','),
+			'venue_category_string' => $venue_category_names->implode(', '),
 			'nearby_venues' => $nearby_venues
 		]);
 	}
