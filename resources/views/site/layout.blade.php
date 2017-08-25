1f6039245ecf947cc50268842cd6ff71133b351b
Alan Sprecacenere <alan@qreate.it>
2017-08-25 17:37:51 +0200

    Aggiunte favicon per i browser generici e quelle specifiche per iPhone e Safari

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 	<head>
+		{{-- Metas --}}
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 
+		{{-- Title --}}
 		<title>
 			@hasSection('title')
 				@yield('title') - {{ config('app.name') }}
@@ -13,11 +15,19 @@
 			@endif
 		</title>
 
+		{{-- Typekit script --}}
 		<script src="https://use.typekit.net/qwv3xzz.js"></script>
 		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
+		{{-- Styles --}}
 		<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 		@yield('stylesheets')
+
+		{{-- Favicons --}}
+		<link rel="apple-touch-icon" href="/img/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/16x16.png">
+		<link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#7dd194">
 	</head>
 	<body class="page @yield('body_class')">
 		<div id="app">
