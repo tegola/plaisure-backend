b8f092d10fb05ed262d82505193840017741011e
Alan Sprecacenere <alan@qreate.it>
2018-01-27 03:19:35 +0100

    Rimosso Babili per il build di librerie es6

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,41 +1,29 @@
 const mix = require('laravel-mix');
-const BabiliPlugin = require('babili-webpack-plugin'); // FIXME: Not needed anymore
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const webpackConfig = {
-	plugins: []
-};
+mix.setPublicPath('public')
+	.autoload({
+		jquery: ['$', 'jQuery'], // Bootstrap
+		'popper.js': 'Popper', // Bootstrap
+	});
 
-mix.autoload({
-	jquery: ['$', 'jQuery'], // Bootstrap
-	'popper.js': 'Popper', // Bootstrap
-});
+
+if (!mix.inProduction()) {
+	mix.webpackConfig({
+		plugins: [
+			new BundleAnalyzerPlugin({
+				analyzerPort: 8889
+			})
+		]
+	});
+}
 
 // Admin
-mix.sass('resources/assets/sass/admin/main.scss', 'public/css/admin.css')
-	.js('resources/assets/js/admin/main.js', 'public/js/admin.js')
+mix.sass('resources/assets/sass/admin/main.scss', 'css/admin.css')
+	.js('resources/assets/js/admin/main.js', 'js/admin.js')
 	.version();
 
 // App
-mix.sass('resources/assets/sass/app/main.scss', 'public/css/app.css')
-	.js('resources/assets/js/app/main.js', 'public/js/app.js')
+mix.sass('resources/assets/sass/app/main.scss', 'css/app.css')
+	.js('resources/assets/js/app/main.js', 'js/app.js')
 	.version();
-
-
-if (mix.inProduction()) {
-	// Use Babili instead of UglifyJS, since it won't transpile ES6
-	mix.options({
-		uglify: false
-	});
-	webpackConfig.plugins.push(
-		new BabiliPlugin()
-	);
-} else {
-	webpackConfig.plugins.push(
-		new BundleAnalyzerPlugin({
-			analyzerPort: 8889
-		})
-	);
-}
-
-mix.webpackConfig(webpackConfig);
