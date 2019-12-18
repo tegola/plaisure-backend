eaf81d6f930b2f07be3d860a0bab62eecb563279
Alan Sprecacenere <alan@qreate.it>
2019-12-19 00:09:55 +0100

    Rimossa query per passare gli id delle attività a HashIds

--- database/migrations/2018_03_25_160909_add_id_hashed_column_to_venues_table.php
+++ database/migrations/2018_03_25_160909_add_id_hashed_column_to_venues_table.php
@@ -3,10 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use App\Models\Venue;
-use Hashids\Hashids;
-use Illuminate\Support\Facades\DB;
 
 class AddIdHashedColumnToVenuesTable extends Migration
 {
@@ -29,22 +25,6 @@ class AddIdHashedColumnToVenuesTable extends Migration
 				->nullable()
 				->unique();
 		});
-
-		// Update existing venues
-		$console = new ConsoleOutput();
-		$hasher = new Hashids(Venue::class, 10);
-
-		$console->write('Updating existing venues with hashed IDs... ');
-
-		$rows = DB::table('venues')->get(['id']);
-		foreach ($rows as $row) {
-			DB::table('venues')
-				->where('id', $row->id)
-				->update(['id_hashed' => $hasher->encode($row->id)]);
-		}
-
-		$console->write('Done!');
-		$console->writeln('');
 	}
 
 	/**
