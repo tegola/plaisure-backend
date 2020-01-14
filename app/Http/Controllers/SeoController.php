dc36313549e71ce6f9d1b5fcda82a97a37960d43
Alan Sprecacenere <alan@qreate.it>
2020-01-14 19:14:54 +0100

    Aggiunte le pagine italiane alla sitemap

--- app/Http/Controllers/SeoController.php
+++ app/Http/Controllers/SeoController.php
@@ -23,23 +23,46 @@ class SeoController extends Controller
 		// Build if not cached
 		if (!$sitemap->isCached()) {
 			// Home page
-			$sitemap->add(route('home'), null, '1.0', 'weekly');
+			$sitemap->add(route('home'), null, '1.0', 'weekly', [], null, $this->sitemapTranslatedUrls('home'));
 
 			// Venues
-			$venues = Venue::all();
-			foreach ($venues as $venue) {
-				$route = route('venues.detail', compact('venue'));
-				$sitemap->add($route, $venue->updated_at, 0.9, 'daily');
+			foreach (Venue::all() as $venue) {
+				$name = 'venues.detail';
+				$params = compact('venue');
+				$sitemap->add(route($name, $params), $venue->updated_at, 0.9, 'daily', [], null, $this->sitemapTranslatedUrls($name, $params));
 			}
 
-			// About, Promote, Play responsibly
-			$sitemap->add(route('about'), null, 0.9, 'monthly');
-			$sitemap->add(route('promote'), null, 0.9, 'weekly');
-			$sitemap->add(route('play-responsibly'), null, 0.9, 'weekly');
+			// About, Promote
+			$sitemap->add(route('about'), null, 0.9, 'monthly', [], null, $this->sitemapTranslatedUrls('about'));
+			$sitemap->add(route('promote'), null, 0.9, 'weekly', [], null, $this->sitemapTranslatedUrls('promote'));
+			$sitemap->add(route('play-responsibly'), null, 0.9, 'weekly', [], null, $this->sitemapTranslatedUrls('play-responsibly'));
 		}
 
 		// Generate XML
 		return $sitemap->render('xml');
+	}
+
+	/**
+	 * Build the translated urls of the specified route for all supported
+	 * languages.
+	 *
+	 * @param  string $name
+	 * @param  array  $params
+	 * @return array
+	 */
+	private function sitemapTranslatedUrls(string $name, $params = [])
+	{
+		$additionalLocales = ['it'];
+		$urls = [];
+
+		foreach ($additionalLocales as $locale) {
+			$urls[] = [
+				'language' => $locale,
+				'url' => route($name, array_merge(['locale' => $locale], $params))
+			];
+		}
+
+		return $urls;
 	}
 
 	/**
