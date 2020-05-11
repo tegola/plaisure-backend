db600d1192d6ad2f79f7022dac115f70307d1624
Alan Sprecacenere <alan@qreate.it>
2020-05-11 20:44:01 +0200

    Upgrade a laravel 7.x

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,10 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use App\Notifications\Admin\ExceptionOccurred as ExceptionOccurredNotification;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Notification;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -31,10 +31,10 @@ class Handler extends ExceptionHandler
 	/**
 	 * Report or log an exception.
 	 *
-	 * @param  \Exception  $exception
+	 * @param  \Throwable  $exception
 	 * @return void
 	 */
-	public function report(Exception $exception)
+	public function report(Throwable $exception)
 	{
 		// Route notifications via email
 		if ($this->shouldReport($exception) && !app()->isLocal()) {
@@ -49,10 +49,10 @@ class Handler extends ExceptionHandler
 	 * Render an exception into an HTTP response.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $exception
+	 * @param  \Throwable  $exception
 	 * @return \Illuminate\Http\Response
 	 */
-	public function render($request, Exception $exception)
+	public function render($request, Throwable $exception)
 	{
 		return parent::render($request, $exception);
 	}
